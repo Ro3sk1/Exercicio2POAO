eab2d9c7c8a09a91ecb07ed9735b4d9fbed54676
Ro3sk1 <61096405+Ro3sk1@users.noreply.github.com>
2024-10-31 14:36:50 +0000

    .

--- Main.java
+++ Main.java
@@ -1,4 +1,29 @@
-public class Main {
-    public static void main(String[] args) {}
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        int escolha_utilizador = -1;
+
+        while (escolha_utilizador != 0) {
+            System.out.print("""
+                    ------- MENU -------
+                    1 . Mostrar todos os computadores
+                    2 . Mostrar todos os computadores com arquitetura x64
+                    3 . Calcular consumo energético
+                    0 . TERMINAR PROGRAMA
+                    --------------------
+                    Introduza a sua opção:\s""");
+
+            Scanner sc = new Scanner(System.in);
+            escolha_utilizador = sc.nextInt();
+
+            switch (escolha_utilizador) {
+                case 1:
+                    System.out.println("a");
+                case 2:
+                    System.out.println("a");
+            }
+        }
+        System.out.println("A terminar o programa...");
+    }
 }
--- RaspberryPi.java
+++ RaspberryPi.java
@@ -1,2 +1,19 @@
 public class RaspberryPi extends NCSLab {
+    RaspberryPi(String nivel, long ram, long disco, float cpu, String arquitetura) {
+        if(nivel.equals("Cloud")) {
+            this.nivel = 1;
+        } else if(nivel.equals("Edge")) {
+            this.nivel = 2;
+        } else if (nivel.equals("IoT")) {
+            this.nivel = 3;
+        }
+        this.ram = ram;
+        this.disco = disco;
+        this.cpu = cpu;
+        if(arquitetura.equals("x64")) {
+            this.arquitetura = true;
+        } else if(arquitetura.equals("ARM")) {
+            this.arquitetura = false;
+        }
+    }
 }
